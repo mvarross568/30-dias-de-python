c9b2ab062b5f6a02672c8a1026277b784b8d760c
mvarross568 <mariavarelarosado2@gmail.com>
2022-12-15 08:53:34 +0100

    sigo trabajando el dia dos

---
+++ day_2/variables.py
@@ -0,0 +1,11 @@
+# Day 2: 30 Days of python programming
+first_name = "Maria"
+last_name = "Varela"
+full_name = "Maria Varela"
+country = "España"
+city = "Jerez"
+age = 16
+year = 2022
+is_married = False
+is_true = True
+is_light_on = True
