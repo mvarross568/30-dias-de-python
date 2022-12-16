95f54fe18e243abed9bc59262916f9001d299a02
mvarross568 <mariavarelarosado2@gmail.com>
2022-12-16 11:15:02 +0100

    he terminado el dia2

--- day_2/variables.py
+++ day_2/variables.py
@@ -9,3 +9,34 @@ year = 2022
 is_married = False
 is_true = True
 is_light_on = True
+print(country , city)
+
+print(type(full_name))
+prin (len(full_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 2
+
+total = num_one+num_two
+print(total)
+
+diff = num_one-num_two
+print(total)
+
+multiply = num_one*num_two
+print(multiply)
+
+divide = num_one/num_two
+print(divide)
+
+r = 30
+pi = 3,14
+diametro = 30*2
+areafcircle = pi*r
+circunferenciacirculo = pi*diametro
+
+print(areafcircle, circunferenciacirculo)
+
+run
+help("keywords")
