e69a71ac7b8ab7edc5b959c9540f0bce6c48bc76
mvarross568 <mariavarelarosado2@gmail.com>
2023-01-19 08:38:18 +0100

    he seguido con el dia3

--- day_3/operations.py
+++ day_3/operations.py
@@ -1,5 +1,17 @@
 age = 16 #1
 height = 1.68 #2
 complex_number = 4j #3
-base = input("Cual es la base: ")
-print(f"La base es {base}")
+b = input("Cual es la base: ")
+print(f"La base es {b}")
+h = input("Cual es la altura: ")
+print(f"La altura es {h}")
+area_triangle = o.5 * b * h #4
+
+a = input("Cual es el valor del lado a: ") #5
+print(f"El valor del lado a es {a}")
+b = input("Cual es el valor del lado b: ")
+print(f"El valor del lado b es {b}")
+c = input("Cual es el valor del lado c ")
+print(f"El valor del lado c es {c}")
+perimetrer = a + b + c
+print(perimetrer)
