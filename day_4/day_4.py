183ba717c5c4186e19138eab4682cc5147bc2b9b
mvarross568 <mariavarelarosado2@gmail.com>
2023-01-27 11:01:57 +0100

    he continuado con el dia4

---
+++ day_4/day_4.py
@@ -0,0 +1,29 @@
+thirty = "Thirty" #1
+days = "Days"
+of = "Of"
+python = "Python"
+space = " "
+full_string = thirty + space + days + space + of + space + python
+print(full_string)
+
+coding = "Coding" #2
+f0r = "for"
+all = "all"
+space = " "
+full_string_2 = coding + space + f0r + space + all
+print(full_string_2)
+
+company = full_string_2 #3
+print(company) #4
+len(print(company)) #5
+print(company.upper()) #6
+print(company.lower()) #7
+
+print(company.capitalize()) #8
+print(company.title())
+print(company.swapcase())
+
+first_slice = company[0:3] #9
+
+
+
