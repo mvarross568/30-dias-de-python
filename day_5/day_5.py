01736266a512efe897f0930d3f6450e4bde02ed6
mvarross568 <117345282+mvarross568@users.noreply.github.com>
2023-02-10 22:31:58 +0100

    continuando dia5

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -3,3 +3,34 @@ print(len(empty_list))
 list_1 = ["1", "2", "3", "4", "5"] #2
 
 
+
+
+
+
+
+print() #9
+it_companies[1] = "vodafone" #10
+print(it_companies)
+
+it_companies.apped("Microsoft") #11
+it_companies.insert(4, "Lenovo") #12
+
+
+
+
+it_companies.sort() #16
+it_companies.reverse() #17
+it_companies.pop() #18
+
+
+it_companies.pop(1) #21
+it_companies.pop(4) #22
+it_companies.pop(8) #23
+it_companies.clear() #24
+
+
+front_end = ["HTML", "CSS", "JS", "React", "Redux" ] #26
+back_end = ["Node", "Express", "MongoDB"]
+
+front_end = full_stack.copy() #27
+back_end = full_stack.copy()
