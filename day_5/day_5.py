ba8793e2b9aad758652955453312d7f46f4d2c2c
mvarross568 <mariavarelarosado2@gmail.com>
2023-02-16 08:29:16 +0100

    he seguido con el dia6

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,5 +1,29 @@
 empty_list= [] #1
 print(len(empty_list))
+
 list_1 = ["1", "2", "3", "4", "5"] #2
+print(len(list_1)) #3
+
+first_list1 = list_1 [0] 
+middle_list1 = list_1 [2]
+last_list1 = list_1 [4] #4
+
+mixed_data_types = ["maria", "16", "1.70", "single", "jerez"] #5
+it_companies = ["initial values", "Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle Amazon"]
+
+print("initial values") #7
+print("Facebook")
+print("Google")
+print("Microsoft")
+print("Apple")
+print("IBM")
+print("Oracle Amazon")
+
+len(it_companies) #8
+
+first_it_companies = it_companies [0] 
+middle_it_companies =  [2]
+last_it_companies =  [4] #9
+
 
 
