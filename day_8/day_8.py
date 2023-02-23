7b9436effe7c56d136c6674568dc471a48e39560
mvarross568 <mariavarelarosado2@gmail.com>
2023-02-23 09:09:49 +0100

    he seguido con el dia8

---
+++ day_8/day_8.py
@@ -0,0 +1,6 @@
+dog = {} #1
+dog ["name"] = "lusytolo" #2
+dog ["breed"] = "retriever"
+dog ["legs"] = "4"
+dog ["age"] = "5"
+student_dictionary = {"first_name": "maria", "last_name": "varela",  "gender": "Female", "age":"16", "marital status":"single", "skills": "creativity", "country": "spain", "city": "jerez","address":"s.josefina"} #3
