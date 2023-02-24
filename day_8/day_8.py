07d1704b16ce7018650227166ea47865dbdfb425
mvarross568 <mariavarelarosado2@gmail.com>
2023-02-24 10:54:36 +0100

    he seguido el dia8

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -4,3 +4,12 @@ dog ["breed"] = "retriever"
 dog ["legs"] = "4"
 dog ["age"] = "5"
 student_dictionary = {"first_name": "maria", "last_name": "varela",  "gender": "Female", "age":"16", "marital status":"single", "skills": "creativity", "country": "spain", "city": "jerez","address":"s.josefina"} #3
+print(len(dog)) #4
+value_of_skills = student_dictionary ["skills"].values() #5
+type(student_dictionary["skills"]) 
+student_dictionary["skills"].append("leadership","responsibility") #6
+keys = dog.keys #7
+print(keys)
+values = dog.values #8
+
+
