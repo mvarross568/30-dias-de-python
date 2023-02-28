e8638de7c825acc115930542671a0af4d23cb3eb
mvarross568 <117345282+mvarross568@users.noreply.github.com>
2023-02-28 13:45:51 +0100

    subido el dia6

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,1 +1,11 @@
 #tengo que subirlo desde mi ordenador
+empty_tuple = ()
+print(empty_tuple)
+tp1 = ("Pedro", "Noelia")
+print(tp1)
+siblings = tp1
+print(siblings)
+len(siblings)
+parents = ("Sonia", "Andrés")
+family_members = parents + siblings
+print(family_members)
